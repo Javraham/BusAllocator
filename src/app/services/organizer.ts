61b78ed82f241c0b90a6074a365f55475ce53ee3
Jonathan Avraham <avrahamjonathan@gmail.com>
2024-08-14 02:47:50 -0400

    fix split passengers algorithm

--- src/app/services/organizer.ts
+++ src/app/services/organizer.ts
@@ -64,35 +64,35 @@ export class TourOrganizer {
 
   getSplitSortedLocation(index: number) {
     const sortedArray = this.getSortedLocation();
-    const randomElement = sortedArray[index];
-    const [location, passengerList] = randomElement;
+    const [location, passengerList] = sortedArray[0];
     const result = sortedArray.filter(pickup => pickup[0] !== location)
-    function splitArrayIntoTwoEvenly(array: Passenger[]): [Passenger[], Passenger[]] {
+    function splitArrayIntoTwoEvenly(array: Passenger[], index: number): [Passenger[], Passenger[]] {
       // Calculate the midpoint index
       const midpoint = Math.ceil(array.length / 2);
 
       // Split the array into two subarrays
-      const subarray1 = array.slice(0, midpoint);
-      const subarray2 = array.slice(midpoint);
-
+      const subarray1 = array.slice(0, index);
+      const subarray2 = array.slice(index);
+      console.log([subarray1, subarray2])
       return [subarray1, subarray2];
     }
 
-    const splitList = splitArrayIntoTwoEvenly(passengerList)
+    const splitList = splitArrayIntoTwoEvenly(passengerList, index)
     result.push([location, splitList[0]])
     result.push([location, splitList[1]])
 
-    return result.sort((a, b) => this.getNumOfPassengersByPickup(b[0]) - this.getNumOfPassengersByPickup(a[0]))
+    return result.sort((a, b) => b[1].reduce((total, currentValue) => total + currentValue.numOfPassengers, 0) - a[1].reduce((total, currentValue) => total + currentValue.numOfPassengers, 0))
   }
 
-  allocatePassengers(sortedLocations = this.getSortedLocation(), numOfTries: number = 0, isSplit: boolean = false): [boolean, boolean] {
+  allocatePassengers(sortedLocations = this.getSortedLocation(), numOfTries: number = 1, isSplit: boolean = false): [boolean, boolean] {
     try {
       const totalCapacities = this.buses.reduce((bus, currentBus) => bus + currentBus.capacity, 0)
       const totalPassengers = sortedLocations.reduce((val, current) => {
         return val + current[1].reduce((passenger, currentPassenger) => passenger + currentPassenger.numOfPassengers, 0)
       }, 0)
+      console.log(sortedLocations, sortedLocations[0][1].length-1)
 
-      if(totalCapacities + 1 < totalPassengers || numOfTries == sortedLocations.length-1){
+      if(totalCapacities + 1 < totalPassengers || numOfTries == this.getSortedLocation()[0][1].length){
         return [false, false]
       }
 
@@ -110,12 +110,12 @@ export class TourOrganizer {
 
         const [location, passengers] = sortedLocations[index];
         const availableBuses = this.buses.filter(bus =>
-          bus.getCurrentLoad() + this.getNumOfPassengersByPickup(location) <= bus.capacity);
+          bus.getCurrentLoad() + passengers.reduce((total, currentValue) => total+currentValue.numOfPassengers, 0) <= bus.capacity);
 
         shuffle(availableBuses); // Shuffle the available buses
 
         for (const bus of availableBuses) {
-          if (bus.getCurrentLoad() + this.getNumOfPassengersByPickup(location) <= bus.capacity) {
+          if (bus.getCurrentLoad() + passengers.reduce((total, currentValue) => total+currentValue.numOfPassengers, 0) <= bus.capacity) {
             passengers.forEach(passenger => bus.addPassenger(passenger));
             if (allocate(index + 1)) {
               return true;
