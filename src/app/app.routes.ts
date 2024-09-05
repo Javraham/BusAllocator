cf3267a5bdc8d0b9cf6a012365b576e2c89aec8f
Jonathan Avraham <avrahamjonathan@gmail.com>
2024-09-05 10:16:04 -0400

    add settings page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {BusAutomationComponent} from "./bus-automation/bus-automation.component";
 import {EmailAutomationComponent} from "./email-automation/email-automation.component";
+import {SettingsPageComponent} from "./settings-page/settings-page.component";
 
 export const routes: Routes = [
   {
@@ -12,5 +13,10 @@ export const routes: Routes = [
     path: "email-automation",
     component: EmailAutomationComponent,
     title: "Email Automation Page"
+  },
+  {
+    path: "settings",
+    component: SettingsPageComponent,
+    title: "Settings"
   }
 ];
