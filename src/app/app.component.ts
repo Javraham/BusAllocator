5e311ba300f8715edf79dfff12b96febe689c38a
Jonathan Avraham <avrahamjonathan@gmail.com>
2024-09-10 12:10:32 -0400

    change nav bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2, ViewChild} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {BusService} from "./services/bus.service";
 import {PassengerComponent} from "./passenger/passenger.component";
@@ -19,18 +19,48 @@ import {OptionsService} from "./services/options.service";
 export class AppComponent implements OnInit{
   @ViewChild('navbar') navbar!: ElementRef;
   @ViewChild('content') content!: ElementRef;
+  @ViewChild('navList', { static: false }) navList!: ElementRef;
+  menuList = document.getElementById("navList") as HTMLElement;
 
-  constructor(private pickupService: PickupsService, private busService: BusService, private optionService: OptionsService) {
+  constructor(private renderer: Renderer2, private pickupService: PickupsService, private busService: BusService, private optionService: OptionsService) {
   }
 
   ngAfterViewInit() {
     const navbarHeight = this.navbar.nativeElement.offsetHeight;
+    const mediaQuery = window.matchMedia('(max-width: 600px)');
     this.content.nativeElement.style.marginTop = `${navbarHeight}px`;
+
+    if (mediaQuery.matches) {
+      // If the media query matches, apply the top value dynamically to the ul
+      this.renderer.setStyle(this.navList.nativeElement, 'top', `${navbarHeight}px`);
+    }
+
+    // Listen for changes in screen width (for responsiveness)
+    mediaQuery.addEventListener('change', (event) => {
+      if (event.matches) {
+        // If the screen width goes below 600px, set the top value
+        this.renderer.setStyle(this.navList.nativeElement, 'top', `${navbarHeight}px`);
+      } else {
+        // If the screen width goes above 600px, remove the top value
+        this.renderer.removeStyle(this.navList.nativeElement, 'top');
+      }
+    });
+
+    this.navList.nativeElement.style.maxHeight = '0px';
   }
 
   ngOnInit() {
     this.pickupService.setPickupLocations()
     this.busService.setBuses()
     this.optionService.setOptions()
+  }
+
+  toggleMenu() {
+    const currentMaxHeight = this.navList.nativeElement.style.maxHeight;
+    if (currentMaxHeight === '0px' || currentMaxHeight === '') {
+      this.renderer.setStyle(this.navList.nativeElement, 'maxHeight', '300px');
+    } else {
+      this.renderer.setStyle(this.navList.nativeElement, 'maxHeight', '0px');
+    }
   }
 }
