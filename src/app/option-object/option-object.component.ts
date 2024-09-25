bbcd70779aadae683d376d4c883621ed198db8ca
Jonathan Avraham <avrahamjonathan@gmail.com>
2024-09-25 15:14:09 -0400

    fix priority bug

--- src/app/option-object/option-object.component.ts
+++ src/app/option-object/option-object.component.ts
@@ -1,10 +1,13 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {ISettingOptionsInput} from "../typings/ISettingOptionsInput";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-option-object',
   standalone: true,
-  imports: [],
+  imports: [
+    NgIf
+  ],
   templateUrl: './option-object.component.html',
   styleUrl: './option-object.component.css'
 })
