080a659b185fd69599137fa0bcefc04e45a47633
Jonathan Avraham <avrahamjonathan@gmail.com>
2024-11-05 08:52:52 -0500

    add copy

--- src/app/bus-automation/bus-automation.component.ts
+++ src/app/bus-automation/bus-automation.component.ts
@@ -17,6 +17,7 @@ import {PickupsService} from "../services/pickups.service";
 import {lastValueFrom} from "rxjs";
 import {IPickup} from "../typings/ipickup";
 import {ActivatedRoute, Router} from "@angular/router";
+import {CdkCopyToClipboard} from "@angular/cdk/clipboard";
 
 @Component({
   selector: 'app-bus-automation',
@@ -27,7 +28,8 @@ import {ActivatedRoute, Router} from "@angular/router";
     NgForOf,
     NgIf,
     PassengerComponent,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CdkCopyToClipboard
   ],
   templateUrl: './bus-automation.component.html',
   styleUrl: './bus-automation.component.css'
